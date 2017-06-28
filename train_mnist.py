d4dc20dd19aea3d1397526ea7e5168fd47b8552f
Mocchi tam <onlyephem@gmail.com>
2017-06-28 16:23:24 +0900

    minor change

--- train_mnist.py
+++ train_mnist.py
@@ -7,30 +7,26 @@ import chainer.links as L
 import chainer.functions as F
 from chainer.dataset import convert
 
-#import numpy as np
-
 def main():
     parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-    parser.add_argument('--batchsize', '-b', type=int, default=100,
+    parser.add_argument('--batchsize', '-b', type=int, default=256,
                         help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=20,
+    parser.add_argument('--epoch', '-e', type=int, default=10,
                         help='Number of sweeps over the dataset to train')
     parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--resume', '-r', default='',
-                        help='Resume the training from snapshot')
-    parser.add_argument('--unit', '-u', type=int, default=1000,
-                        help='Number of units')    
+    parser.add_argument('--train', '-t', type=int, default=1,
+                        help='If negative, skip training')
+    parser.add_argument('--resume', '-r', type=int, default=1,
+                        help='If positive, resume the training from snapshot')
     args = parser.parse_args()
     
     print('GPU: {}'.format(args.gpu))
-    print('# unit: {}'.format(args.unit))
     print('# Minibatch-size: {}'.format(args.batchsize))
     print('# epoch: {}'.format(args.epoch))
-    print('')
     
     flag_train = False if args.train < 0 else True
-    flag_resum = False if args.use < 0 else True
+    flag_resum = False if args.resume < 0 else True
     n_epoch = args.epoch if flag_train == True else 1
     
     tsm = MTModel(args.gpu, flag_train, flag_resum, n_epoch, args.batchsize)
@@ -54,7 +50,8 @@ class MTNNet(chainer.Chain):
     def loss(self, x, t):
         y = self(x)
         loss = F.softmax_cross_entropy(y,t)
-        return loss, y
+        self.accuracy = F.accuracy(y,t)
+        return y, loss
 
 class MTModel():
     def __init__(self, gpu, flag_train, flag_resum, n_epoch, batchsize):
@@ -74,8 +71,14 @@ class MTModel():
             self.optimizer.setup(self.model)
         
         if self.flag_resum:
-            chainer.serializers.load_npz("./net/net.model", self.model)
+            try: 
+                chainer.serializers.load_npz('./net/net.model', self.model)
+                chainer.serializers.load_npz('./net/net.state', self.optimizer)
+                print('successfully resume model')
+            except:
+                print('ERROR: cannot resume model')
         
+        # prepare dataset
         train, test = chainer.datasets.get_mnist()
         
         self.N_train = len(train)
@@ -87,42 +90,55 @@ class MTModel():
                                                           repeat=False, shuffle=False)
         
     def run(self):
-        #xp = np if self.gpu < 0 else chainer.cuda.cupy
         sum_accuracy = 0
         sum_loss = 0
         
         while self.train_iter.epoch < self.n_epoch:
+            # train phase
             batch = self.train_iter.next()
-            x_array, t_array = convert.concat_examples(batch, self.gpu)
-            x = chainer.Variable(x_array)
-            t = chainer.Variable(t_array)
-            optimizer.update(model, x, t)
-            sum_loss += float(model.loss.data) * len(t.data)
-            sum_accuracy += float(model.accuracy.data) * len(t.data)
+            if self.flag_train:
+                # step by step update
+                x, t = convert.concat_examples(batch, self.gpu)
+                
+                self.model.cleargrads()
+                y, loss = self.model.loss(x, t)
+                loss.backward()
+                self.optimizer.update()
+                
+                sum_loss += float(loss.data) * len(t.data)
+                sum_accuracy += float(self.model.accuracy.data) * len(t.data)
             
-            if train_iter.is_new_epoch:
-                print('epoch: ', train_iter.epoch)
+            # test phase
+            if self.train_iter.is_new_epoch:
+                print('epoch: ', self.train_iter.epoch)
                 print('train mean loss: {}, accuracy: {}'.format(
-                    sum_loss / train_count, sum_accuracy / train_count))
-                # evaluation
-                sum_accuracy = 0
-                sum_loss = 0
-                for batch in test_iter:
-                    x_array, t_array = convert.concat_examples(batch, args.gpu)
-                    x = chainer.Variable(x_array)
-                    t = chainer.Variable(t_array)
-                    loss = model(x, t)
-                    sum_loss += float(loss.data) * len(t.data)
-                    sum_accuracy += float(model.accuracy.data) * len(t.data)
-    
-                test_iter.reset()
-                print('test mean  loss: {}, accuracy: {}'.format(
-                    sum_loss / test_count, sum_accuracy / test_count))
+                        sum_loss / self.N_train, sum_accuracy / self.N_train))
+                
                 sum_accuracy = 0
                 sum_loss = 0
                 
-        chainer.serializers.save_npz('mlp.model', self.model)
-        chainer.serializers.save_npz('mlp.state', self.optimizer)
+                for batch in self.test_iter:
+                    x, t = convert.concat_examples(batch, self.gpu)
+                    
+                    with chainer.using_config('train', False), chainer.no_backprop_mode():
+                        y, loss = self.model.loss(x, t)
+                        
+                    sum_loss += float(loss.data) * len(t.data)
+                    sum_accuracy += float(self.model.accuracy.data) * len(t.data)
+                    
+                self.test_iter.reset()
+                print('test mean  loss: {}, accuracy: {}'.format(
+                        sum_loss / self.N_test, sum_accuracy / self.N_test))
+                
+                sum_accuracy = 0
+                sum_loss = 0
+        
+        try:
+            chainer.serializers.save_npz('net/net.model', self.model)
+            chainer.serializers.save_npz('net/net.state', self.optimizer)
+            print('Successfully saved model')
+        except:
+            print('ERROR: saving model ignored')
 
 if __name__ == '__main__':
     main()
